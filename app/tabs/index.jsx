0794593d6f8401f540619edd83a888f68967521c
luka kljun <luka.kljun@students.vegova.si>
2024-06-25 20:13:07 +0200

    i hope it works (id doesnt)

--- app/tabs/index.jsx
+++ app/tabs/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { View, Text, Button, SafeAreaView, ScrollView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
-import { COLORS, icons, SIZES, FONTS, images } from '../constants';
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+import { COLORS, icons, SIZES, FONTS, images } from '../../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../../components';
 
 const Home = () => {
     const router = useRouter();
@@ -12,7 +12,7 @@ const Home = () => {
             <Stack.Screen
             options={{
                 headerStyle: { backgroundColor: COLORS.lightWhite },
-                headerShadowVisible: false,
+                headerShadowVisible: true,
                 headerLeft: () => (
                     <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
                 ),
@@ -22,6 +22,19 @@ const Home = () => {
                 headerTitle: ""
             }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                    }}
+                >
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
